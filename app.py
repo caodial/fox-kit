51845cbcbdfc239052ef4efdc8214db3e294c17c
Chaikou D <104278113+caodial@users.noreply.github.com>
2024-04-24 20:37:12 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,8 @@
+print("--------Foxkit---------")
+print("Version de développement")
+def notepad():
+    print("notepad.exe")
+prompt = input("Qu'est-ce que vous voulez faire?")
+appedit = print("Opening app editor")
+notepad()
+if prompt : appedit
