c75ffc3435cb0dc2428e8631b9e54305341ef3e4
caodial <caodiall@gmail.com>
2024-10-28 16:25:10 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,12 @@
 echo ---Foxkit installer---
+
+echo "Do you want to continue? (y/n)"
+read answer
+
+if [ "$answer" == "y" ]; then
+    
+elif [ "$answer" == "no" ]; then
+    echo "You chose no!"
+else
+    echo "Invalid answer. Please respond with 'yes' or 'no'."
+fi
