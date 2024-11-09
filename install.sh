a18db738371a70a6af1650ab9b0295149a2b73bc
caodial <caodiall@gmail.com>
2024-11-09 12:16:32 -0500

    Added code

--- install.sh
+++ install.sh
@@ -4,9 +4,12 @@ echo "Do you want to continue? (y/n)"
 read answer
 
 if [ "$answer" == "y" ]; then
-    
+    sudo apt-get update && sudo apt-get upgrade
+    sudo apt-get install git
+    git pull http://localhost:3000/caodial/Foxkit
+    echo Foxkit has sucssefuly installed
 elif [ "$answer" == "no" ]; then
-    echo "You chose no!"
+    exit
 else
-    echo "Invalid answer. Please respond with 'yes' or 'no'."
+    echo "Invalid answer. Please respond with 'y' or 'n'."
 fi
