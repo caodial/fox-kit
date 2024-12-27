c29413c03c7a35319dea57610c8636cb108708cf
caodial <caodiall@gmail.com>
2024-12-26 19:00:29 -0500

    Update foxkit-post-install.sh

--- foxkit-post-install.sh
+++ foxkit-post-install.sh
@@ -65,9 +65,6 @@ echo "MySQL installation and setup complete."
 echo "Please run the following command to log into MySQL:"
 echo "mysql -u root -p"
 
-# Open foxkit.sh
-./foxkit.sh
-
 # Wait for user to press any key to exit
 read -n 1 -s -r -p "Press any key to exit"
 
